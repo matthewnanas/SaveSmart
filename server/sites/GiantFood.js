e751025e7c3fdca881999956aaaef1b0cd65e455
matthewnanas <spazeplayz@gmail.com>
2022-11-15 17:03:04 -0500

    Update GiantFood.js

--- server/sites/GiantFood.js
+++ server/sites/GiantFood.js
@@ -1,5 +1,19 @@
 const axios = require("axios");
 
+/**
+ * Conduct item search
+ * 
+ * Conduct price search
+ * 
+ * Match prices to items via id
+ * 
+ * Things needed to query:
+ *  1. query
+ *  2. retailerInventorySessionToken
+ *  3. pageViewId
+ *  4. sha256hash
+ */
+
 class GiantFood {
     constructor(info) {
         this.zip = info.zipCode;
@@ -7,37 +21,86 @@ class GiantFood {
     }
 
     async start() {
-        this.getLocation();
+        this.getRelevant();
     }
 
-    async getLocation() {
-        const response = await axios.post(`https://giantfood.com/api/v5.0/products/1704970509/50000351?elevaateInclude=true&sort=bestMatch+asc&filter=&start=0&flags=true&keywords=milk&nutrition=false&facetExcludeFilter=true&facet=categories,brands,nutrition,sustainability,specials,newArrivals,privateLabel`, {
-            /*headers: {
-                'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-                'X-Forwarded-For': '66.249.79.164',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Cache-Control': 'max-age=0',
-                'Upgrade-Insecure-Requests': '1',
-                'Connection': 'keep-alive',
-            }*/
-            headers: {
-                'sec-ch-device-memory': '8',
-                'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-                'sec-ch-ua-arch': '"arm"',
-                'sec-ch-ua-full-version-list': '"Google Chrome";v="107.0.5304.110", "Chromium";v="107.0.5304.110", "Not=A?Brand";v="24.0.0.0"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"macOS"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-origin',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-                'cookie': 'datadome=ZzillVaBL4mbr89XjZytqel8J8SEWMnNY6RP5jqQcZf60QK0S-Vjk-EFQPVYydydO31l5FHvZT7HztWCYcW8ZLdOr~vifDyKSHcutSihiF3rwbx.MB9QF.v8FCIG1Ac'
-            },
-        });
+    /**
+     * Function getRelevant
+     * 
+     * Use instacart endpoint and filter to find the most relevant item to the search
+     */
+    async getRelevant() {
+        try {
+            // Get item endpoint
+            const response = await axios.get(`https://www.instacart.com/graphql?operationName=SearchWithClustering&variables={%22filters%22:[],%22disableReformulation%22:false,%22retailerInventorySessionToken%22:%22v1.bab5661.2860219233-20906-03908x17705-1-159-5229-0%22,%22orderBy%22:%22default%22,%22query%22:%22${this.item.replace(' ', '%20')}%22,%22pageViewId%22:%2202390ea6-023a-5b3c-9f2c-bd5c9b04e306%22,%22includeDebugInfo%22:false,%22elevatedProductId%22:null}&extensions={%22persistedQuery%22:{%22version%22:1,%22sha256Hash%22:%223a191ccb71dba998291ee4452457cbba4655dcc15c2f440d0bad7f80d540d548%22}}`, {
+                headers: {
+                    'sec-ch-device-memory': '8',
+                    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                    'sec-ch-ua-arch': '"arm"',
+                    'sec-ch-ua-full-version-list': '"Google Chrome";v="107.0.5304.110", "Chromium";v="107.0.5304.110", "Not=A?Brand";v="24.0.0.0"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"macOS"',
+                    'sec-fetch-dest': 'empty',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-site': 'same-origin',
+                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+                    'cookie': `__Host-instacart_sid=v2.aa7b7b61.OCaW4K00T0BlrtiD1HhERuyZGJD7uWKvAm9QYPvkVww;`
+                },
+            });
 
-        console.log(response.data);
-        console.log(response.status)
+            //console.log(response.data['data']['searchResults']['primaryItemResultList']['items'][0]);
+
+            // Check to see if any items exist
+            if (response.data['data']['searchResults']['primaryItemResultList']['items'].length == 0) {
+                console.log('No items found');
+            } else {
+                const firstTen = JSON.stringify(response.data['data']['searchResults']['primaryItemResultList']['itemIds'].slice(0, 10));
+                const relevant = response.data['data']['searchResults']['primaryItemResultList']['items'][0];
+                return this.getRelevant(`https://www.instacart.com/graphql?operationName=ItemPricesQuery&variables={"ids":${firstTen},"shopId":"4192","zoneId":"37","postalCode":"20902"}&extensions={"persistedQuery":{"version":1,"sha256Hash":"6bc7919897d4104897f991ab9e6f544aa2157e60781606871bf236f30e50243f"}}`, relevant);
+            }
+        } catch (err) {
+            console.log(err);
+            console.log('Error sending instacart request');
+        }
+    }
+
+    /**
+     * Function getRelevant
+     * 
+     * Use instacart endpoint to get the price of the most relevant price
+     */
+    async getRelevantPrice(url, relevant) {
+        try {
+            console.log(url);
+            // Get price endpoint
+            const response = await axios.get(url, {
+                headers: {
+                    'sec-ch-device-memory': '8',
+                    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                    'sec-ch-ua-arch': '"arm"',
+                    'sec-ch-ua-full-version-list': '"Google Chrome";v="107.0.5304.110", "Chromium";v="107.0.5304.110", "Not=A?Brand";v="24.0.0.0"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"macOS"',
+                    'sec-fetch-dest': 'empty',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-site': 'same-origin',
+                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+                    'cookie': `__Host-instacart_sid=v2.aa7b7b61.OCaW4K00T0BlrtiD1HhERuyZGJD7uWKvAm9QYPvkVww;`
+                },
+            });
+
+            // Check to see if any items exist
+            if (response.data['data']['itemPrices'].length == 0) {
+                console.log('No prices found');
+            } else {
+                /*for (let x = 0; x < response.data['data']['itemPrices']; x++) {
+                    if (response.data['data']['itemPrices'][x]['id'] == )
+                }*/
+            }
+        } catch (err) {
+            console.log(err);
+            console.log('Error sending instacart request');
+        }
     }
 }
 
