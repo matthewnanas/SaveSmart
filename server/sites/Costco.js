fdb564b677ca97cdf7418f310b76a7835b950fe5
matthewnanas <spazeplayz@gmail.com>
2022-11-15 22:21:13 -0500

    Init costco

---
+++ server/sites/Costco.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * Costco.js
+ * 
+ * This file will mimic a module that grabs the relevant items from the list array and return a structured array of the items
+ * 
+ * Flow:
+ * 1. Conduct item search
+ * 2. Conduct price search
+ * 
+ */
