dfb01480316c9ec5c1a1ac89bc4262ab6a842f56
matthewnanas <spazeplayz@gmail.com>
2022-11-16 20:04:06 -0500

    Started lidl

---
+++ server/sites/Lidl.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * Lidl.js
+ * 
+ * This file will mimic a module that grabs the relevant items from the list array and return a structured array of the items
+ * 
+ * Flow:
+ * 1. Conduct item search
+ * 2. Conduct price search
+ * 
+ */
+
+const axios = require('axios');
+const CookieJar = require('tough-cookie');
+const wrapper = require('axios-cookiejar-support');
+
+class Lidl {
+    constructor(info) {
+        this.items = info.items;
+        this.jar = new CookieJar.CookieJar();
+        this.client = wrapper.wrapper(axios.create({ jar: this.jar }));
+    }
+
+    async compileList() {
+        const items = [];
+
+        // Search for item in items list
+        for (let x = 0; x < this.items.length; x++) {
+            const result = await this.getRelevant(this.items[x]);
+            items.push(result);
+        }
+
+        console.log(items);
+    }
+
+    
+}
+
+const test = new Lidl({
+    items: ['oats', 'whole milk'],
+});
+
+test.compileList();
