9c73356cc2f75588937b68c3a0ce35a3da57a34d
matthewnanas <spazeplayz@gmail.com>
2022-11-17 00:04:43 -0500

    Added shoppers to api

--- server/sites/Shoppers.js
+++ server/sites/Shoppers.js
@@ -19,7 +19,6 @@ class Shoppers {
         this.items = info.items;
         this.jar = new CookieJar.CookieJar();
         this.client = wrapper.wrapper(axios.create({ jar: this.jar }));
-        this.list = [];
     }
 
     async compileList() {
@@ -31,8 +30,7 @@ class Shoppers {
             items.push(result);
         }
 
-        console.log(items);
-        this.list = items;
+        return items;
     }
 
     /**
@@ -114,8 +112,4 @@ class Shoppers {
     }
 }
 
-const test = new Shoppers({
-    items: ['oats', 'whole milk'],
-});
-
-test.compileList();
+module.exports = Shoppers;
