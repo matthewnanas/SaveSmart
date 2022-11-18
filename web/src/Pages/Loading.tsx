2e9ce9379a6e8fa2523b68a2cf457c7228a437d7
matthewnanas <spazeplayz@gmail.com>
2022-11-18 12:15:46 -0500

    Loading page colorization

--- web/src/Pages/Loading.tsx
+++ web/src/Pages/Loading.tsx
@@ -63,7 +63,7 @@ export default function Loading() {
 
         async function getLists() {
             for (var x = 0; x < api.length; x++) {
-                setStatus(`Grabbing prices from ${api[x].storeName}`)
+                setStatus(`${api[x].storeName}`)
                 let response = await fetch(`http://localhost:7777/${api[x].endpoint}`, options);
                 let parsed = await response.json();
                 compiled.push({
@@ -92,7 +92,7 @@ export default function Loading() {
                     className='loadingIcon'
                     // style={{ }}
                 />
-                <h1 style={{marginTop: 55}}>{status}</h1>
+                <h1 style={{marginTop: 55}}>Grabbing price from <span style={{ color: '#1DA1F2' }}>{status}</span></h1>
             </div>
         </div>
     )
