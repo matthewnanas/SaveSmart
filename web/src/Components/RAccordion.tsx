02c25640c45160d17c0ca44b4eafad23d02428fe
matthewnanas <spazeplayz@gmail.com>
2022-11-26 02:42:16 -0500

    New Desktop Accordion

--- web/src/Components/RAccordion.tsx
+++ web/src/Components/RAccordion.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
+import { BsFillEyeFill, BsWallet, BsCaretDownFill, BsInfoLg } from 'react-icons/bs';
 
 import '../Static/Styles/RAccordion.css'
 
@@ -10,6 +12,148 @@ import '../Static/Styles/RAccordion.css'
  * @return a accordion component given props suited for desktop displays
  */
 export function DesktopRAccordion(props: { 
+    name: string,
+    result: any, 
+    estTotal: string,
+    logo: string,
+    catalog: string,
+}) {
+    const [opacity, setOpacity] = React.useState('0');
+    const [display, setDisplay] = React.useState('none');
+    const [line, setLine] = React.useState('#ffff');
+
+    const handleClick = () => {
+        if(opacity === '0'){
+            setOpacity('1')
+            setDisplay('block')
+            setLine('gray')
+        } else {
+            setOpacity('0')
+            setDisplay('none')
+            setLine('#ffff')
+        }
+    };
+
+    useEffect(() => {
+        console.log(props.result)
+    }, [])
+
+    return (
+        <Container className='raccordionContainer'>
+            <Row>
+                <Col>
+                    <button className='ViewCatalog' onClick={() => window.open(props.catalog)}>
+                        <span>View Catalog &nbsp;</span>
+                        <BsFillEyeFill />
+                    </button>
+                </Col>
+                <Col xs={5} style={{textAlign: 'left'}}>
+                    <Row>
+                        <div className='StoreColumn'>
+                            <h6>Store</h6>
+                            <h3>{props.name}</h3>
+                        </div>
+                    </Row>
+                </Col>
+                <Col>
+                    <div className='TotalPrice'>
+                        <BsWallet />
+                        <span> &nbsp;Total Price: {props.estTotal}</span>
+                    </div>
+                </Col>
+                <Col>
+                    <button className='UtilButton' onClick={handleClick}>
+                        <BsCaretDownFill />
+                    </button>
+                    <div className='UtilButton'>
+                        <BsInfoLg />
+                    </div>
+                </Col>
+            </Row>
+            <div className='raccordionBody' style={{ display: display }}>
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>Search Term</th>
+                            <th>Image</th>
+                            <th>Store Item</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.result.map((
+                                item: {
+                                    product_name: string | null,
+                                    product_size: string | null,
+                                    brand: string | null,
+                                    price: string | null,
+                                    unit_price: string | null,
+                                    image: string | undefined,
+                                    query: string,
+                                    link: string | undefined,
+                                    quantity: number,
+                                }, i: any
+                            ) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>
+                                            {
+                                                item?.query?
+                                                    <p>{item.query}</p>
+                                                : <>N/A</>
+                                            }
+                                        </td>
+                                        <td>
+                                            <div className='accordionStore'>
+                                                {
+                                                    item?.image?
+                                                        <img src={item.image} width={50} alt='product'/>
+                                                    : <>N/A</>
+                                                }
+                                            </div>
+                                        </td>
+                                        <td>
+                                            {
+                                                item?.product_name? 
+                                                    <a href={item.link} target="_blank" rel="noreferrer">
+                                                        <p>{item.product_name}</p>
+                                                    </a>
+                                                : <>Not found</>
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                item?.quantity? 
+                                                    <p>{item.quantity}</p>
+                                                : <p>1</p>
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                item?.price? 
+                                                    <p>{item?.price}</p>
+                                                : <>$0.00</>
+                                            }
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </div>
+        </Container>
+    )
+}
+
+/**
+ * @function DesktopRAccordion
+ * 
+ * @return a accordion component given props suited for desktop displays
+ */
+/*export function DesktopRAccordion(props: { 
     name: string,
     result: any, 
     estTotal: string,
@@ -131,7 +275,7 @@ export function DesktopRAccordion(props: {
             </div>
         </div>
     )
-}
+}*/
 
 /**
  * @function MobileRAccordion
