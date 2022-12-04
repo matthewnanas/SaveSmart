29fe1cfc1d85d90f5bb46e279325b3116cf6b259
matthewnanas <spazeplayz@gmail.com>
2022-12-04 02:26:31 -0500

    Mobile accordion card

--- web/src/Components/RAccordion.tsx
+++ web/src/Components/RAccordion.tsx
@@ -238,94 +238,160 @@ export function MobileRAccordion(props: {
     }
 
     return (
-        <div className='raccordionContainerMobile'>
-            <div className='raccordionHeader' onClick={handleClick}>
-                <div className='raccordionStore'>
-                    <p>Store</p>
-                    <a href={props.catalog} target="_blank" rel="noreferrer">
-                        <img src={props.logo} alt={`${props.name} Logo`} />
-                    </a>
-                </div>
-                <div className='raccordionTotal'>
-                    <p>Estimated Total</p>
-                    <h3>{props.estTotal}</h3>
-                </div>
+        <Container className='raccordionContainerMobile'>
+            {/*<Row>
+                <Col style={{whiteSpace: 'nowrap'}}>
+                    <button className='' onClick={() => window.open(props.catalog)}>
+                        <BsFillEyeFill />
+                        <span> &nbsp;View Catalog</span>
+                    </button>
+                </Col>
+                <Col xs={5} style={{textAlign: 'left', whiteSpace: 'nowrap'}}>
+                    <Row>
+                        <div className='StoreColumn'>
+                            <h6>Store</h6>
+                            <h3>{props.name}</h3>
+                        </div>
+                    </Row>
+                </Col>
+                <Col style={{whiteSpace: 'nowrap'}}>
+                    <div className='TotalPrice'>
+                        <BsWallet />
+                        <span> &nbsp;Total Price: ${Math.trunc(total*100)/100}</span>
+                    </div>
+                </Col>
+                <Col>
+                    <button className='UtilButton' onClick={handleClick}>
+                        <BsCaretDownFill />
+                    </button>
+                    <div className='UtilButton'>
+                        <BsInfoLg />
+                    </div>
+                </Col>
+            </Row>*/}
+
+            <div>
+                <Row style={{paddingTop: '10px', paddingBottom: '10px'}}>
+                    <Col style={{display: 'flex', marginLeft: '10px'}}>
+                        <img src={props.logo} width={50} alt='Store'/>
+                        <div style={{marginLeft: '15px'}}>
+                            <h6>Store</h6>
+                            <h3>{props.name}</h3>
+                        </div>
+                    </Col>
+                    <Col />
+                    <Col>
+                        <div className='UtilButtonMobile'>
+                            <BsInfoLg />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={8} style={{textAlign: 'left', whiteSpace: 'nowrap'}}>
+                        <button className='ViewMobile' onClick={handleClick}>
+                            <span>View More</span>
+                        </button>
+                    </Col>
+                    <Col>
+                        <div className='TotalPrice'>
+                            <span>${Math.trunc(total*100)/100}</span>
+                        </div>
+                    </Col>
+                </Row>
             </div>
-            <hr />
+            
             <div className='raccordionBody' style={{ display: display }}>
                 <Table striped>
                     <thead>
                         <tr>
-                            <th className='accordionQuery'>Search Term</th>
+                            <th>Search Term</th>
                             <th>Image</th>
                             <th>Store Item</th>
-                            <th className='accordionQuery'>Quantity</th>
+                            <th>Quantity</th>
                             <th>Price</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {
-                            (props.result || []).map((
-                                item: {
-                                    product_name: string | null,
-                                    product_size: string | null,
-                                    brand: string | null,
-                                    price: string | null,
-                                    unit_price: string | null,
-                                    image: string | undefined,
-                                    query: string,
-                                    link: string | undefined,
-                                    quantity: number,
-                                }, i: any
-                            ) => {
-                                return (
-                                    <tr key={i}>
-                                        <td className='accordionQuery'>
-                                            {
-                                                item?.query?
-                                                    <p>{item.query}</p>
-                                                : <>N/A</>
-                                            }
-                                        </td>
-                                        <td>
-                                            <div className='accordionStore'>
-                                                {
-                                                    item?.image?
-                                                        <img src={item.image} width={50} alt='product'/>
-                                                    : <>N/A</>
-                                                }
-                                            </div>
-                                        </td>
-                                        <td>
-                                            {
-                                                item?.product_name? 
-                                                    <a href={item.link} target="_blank" rel="noreferrer">
-                                                        <p>{item.product_name}</p>
-                                                    </a>
-                                                : <>Not found</>
-                                            }
-                                        </td>
-                                        <td className='accordionQuery'>
-                                            {
-                                                item?.quantity? 
-                                                    <p>{item.quantity}</p>
-                                                : <p>1</p>
-                                            }
-                                        </td>
-                                        <td>
-                                            {
-                                                item?.price? 
-                                                    <p>{item?.price}</p>
-                                                : <>$0.00</>
-                                            }
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
                 </Table>
+
+                {
+                    (props.result || []).map((
+                        item: {
+                            items: any[],
+                            query: string,
+                            quantity: number,
+                        }, i: any
+                    ) => {
+                        return (
+                            <Swiper
+                                navigation
+                                modules={[Navigation]}
+                                onSlideChange={index => editTotal(item.query, index.previousIndex, index.activeIndex)}
+                                key={i}
+                            >
+                                {
+                                    (item.items || []).map((
+                                        product: {
+                                            brand: string | null,
+                                            image: string | null,
+                                            link: string | null,
+                                            price: string | null,
+                                            product_name: string | null,
+                                            product_size: string | null,
+                                            query: string | null,
+                                            unit_price: string | null,
+                                        }, z: any
+                                    ) => {
+                                        return (
+                                            <SwiperSlide key={z}>
+                                                <Row className='ItemSlide'>
+                                                    <Col style={{textAlign: 'right'}}>
+                                                        {
+                                                            product?.query?
+                                                                <p>{product.query}</p>
+                                                            : <>N/A</>
+                                                        }
+                                                    </Col>
+                                                    <Col style={{textAlign: 'right'}}>
+                                                        {
+                                                            product?.image?
+                                                                <img src={product.image} width={50} alt='product'/>
+                                                            : <>N/A</>
+                                                        }
+                                                    </Col>
+                                                    <Col xs={6}>
+                                                        {
+                                                            product?.link? 
+                                                                <a href={product.link} target="_blank" rel="noreferrer">
+                                                                    <p>{product.product_name}</p>
+                                                                </a>
+                                                            : <>Not found</>
+                                                        }
+                                                    </Col>
+                                                    <Col>
+                                                        {
+                                                            item?.quantity? 
+                                                                <p>{item.quantity}</p>
+                                                            : <p>1</p>
+                                                        }
+                                                    </Col>
+                                                    <Col>
+                                                        {
+                                                            product?.price? 
+                                                                <p>{product?.price}</p>
+                                                            : <>$0.00</>
+                                                        }
+                                                    </Col>
+                                                    <Col />
+                                                </Row>
+                                            </SwiperSlide>
+                                        )
+                                    })
+                                }
+                            </Swiper>
+                        )
+                    })
+                }
             </div>
-        </div>
+        </Container>
     )
 }
